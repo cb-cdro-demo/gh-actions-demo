dcb6a1a510be6064dc137c754606d381b17fc721
cb-cdro-demo01 <142387485+cb-cdro-demo01@users.noreply.github.com>
2023-10-30 00:47:20 +0900

    Update application.groovy

--- platform_content/application.groovy
+++ platform_content/application.groovy
@@ -131,7 +131,7 @@ serviceAccount:
         type = 'entry'
       }
 
-      formalParameter 'imageRepository', defaultValue: '${{ secrets.HARBOR_REGISTRY_URL }}' + '/${{ secrets.HARBOR_USER }}/demo-app', {
+      formalParameter 'imageRepository', defaultValue: args.harborRegistryUrl + '/' + args.harborUser + '/demo-app', {
         label = 'Image Repository'
         orderIndex = '1'
         required = '1'
